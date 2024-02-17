8d400bc6d70605fd679d0f2bc83b74963b8bc6c6
Sebastian <goldgreenwhite@hotmail.com>
2024-02-17 17:51:38 +0000

    change name of app

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Continuous Cloud Delivery and Integration";
 	}
 
 	public static void main(String[] args) {
